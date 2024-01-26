e9ecd61ad84f094b3815ae7b3c71fc84ec6a5ffb
jatin <jatinranjan8@gmail.com>
2024-01-26 10:25:10 +0530

    added microphone along side announcement

--- app/components/home_page/announcements.jsx
+++ app/components/home_page/announcements.jsx
@@ -37,11 +37,8 @@ export default function Announcements() {
           fill="currentColor"
           className="w-8 h-8"
         >
-          <path
-            fillRule="evenodd"
-            d="M15.75 4.5a3 3 0 1 1 .825 2.066l-8.421 4.679a3.002 3.002 0 0 1 0 1.51l8.421 4.679a3 3 0 1 1-.729 1.31l-8.421-4.678a3 3 0 1 1 0-4.132l8.421-4.679a3 3 0 0 1-.096-.755Z"
-            clipRule="evenodd"
-          />
+          <path d="M8.25 4.5a3.75 3.75 0 1 1 7.5 0v8.25a3.75 3.75 0 1 1-7.5 0V4.5Z" />
+          <path d="M6 10.5a.75.75 0 0 1 .75.75v1.5a5.25 5.25 0 1 0 10.5 0v-1.5a.75.75 0 0 1 1.5 0v1.5a6.751 6.751 0 0 1-6 6.709v2.291h3a.75.75 0 0 1 0 1.5h-7.5a.75.75 0 0 1 0-1.5h3v-2.291a6.751 6.751 0 0 1-6-6.709v-1.5A.75.75 0 0 1 6 10.5Z" />
         </svg>
       </div>
       <div className=" rounded-2xl flex flex-col items-center justify-between p-2">
